123a615a5dbe24f9c19905503e235c8df2e41b7a
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-08-24 14:36:54 +1200

    Initial setup of js css html files + box creation

---
+++ EAS.js
@@ -0,0 +1,10 @@
+const twelveDivs = document.createElement("div");
+twelveDivs.class = "square"
+let counter = 0;
+while (counter < 20){
+    document.getElementById("container").appendChild(twelveDivs);
+    counter++;
+}
+
+
+
