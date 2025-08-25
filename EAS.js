162e6df90fd308b63b41c24bb4d76e252e361ae5
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-08-25 14:18:17 +1200

    Added javascript for reset button
    Added functionality for dynamic sizing of squares from button prompt

--- EAS.js
+++ EAS.js
@@ -1,15 +1,17 @@
 const container = document.getElementById("container")
 let counter = 0;
-while (counter < 256){
+while (counter < 256) {
     const twelveDivs = document.createElement("div");
     container.appendChild(twelveDivs);
     twelveDivs.classList.add("square");
-    twelveDivs.id ="deActivated";
+    twelveDivs.id = "deActivated";
+    twelveDivs.style.width = "31.25px"
+    twelveDivs.style.height = "31.25px"
     counter++;
     console.log(counter)
 }
 
-const hovered = document.querySelectorAll(".square");
+let hovered = document.querySelectorAll(".square");
 hovered.forEach((square) => {
     square.addEventListener('mouseenter', (event) => {
     let target = event.target;
@@ -17,3 +19,29 @@ hovered.forEach((square) => {
     });
 });
 
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener('click', (event) => {
+    let squareNumber = prompt("how many squares per side of the grid?");
+    let target = event.target;
+    let counter = 0;
+    let size = 500/squareNumber
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    while (counter < (squareNumber * squareNumber)) {
+        const twelveDivs = document.createElement("div");
+        container.appendChild(twelveDivs);
+        twelveDivs.classList.add("square");
+        twelveDivs.id ="deActivated";
+        twelveDivs.style.width = `${size}px`;
+        twelveDivs.style.height = `${size}px`;
+        counter++;
+    }
+    let hovered = document.querySelectorAll(".square");
+    hovered.forEach((square) => {
+        square.addEventListener('mouseenter', (event) => {
+        let target = event.target;
+        target.classList.add("activatedSquare");
+        });
+    });
+});
