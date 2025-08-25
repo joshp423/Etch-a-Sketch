0619a1ab11a3a7eb41f23fb9178a9069c7ddc055
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-08-25 13:17:55 +1200

    Added hover event listener for all squares + css

--- EAS.js
+++ EAS.js
@@ -1,10 +1,19 @@
-const twelveDivs = document.createElement("div");
-twelveDivs.class = "square"
+const container = document.getElementById("container")
 let counter = 0;
-while (counter < 20){
-    document.getElementById("container").appendChild(twelveDivs);
+while (counter < 256){
+    const twelveDivs = document.createElement("div");
+    container.appendChild(twelveDivs);
+    twelveDivs.classList.add("square");
+    twelveDivs.id ="deActivated";
     counter++;
+    console.log(counter)
 }
 
-
+const hovered = document.querySelectorAll(".square");
+hovered.forEach((square) => {
+    square.addEventListener('mouseenter', (event) => {
+    let target = event.target;
+    target.classList.add("activatedSquare");
+    });
+});
 
